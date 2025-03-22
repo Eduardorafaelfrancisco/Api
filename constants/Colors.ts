2e3e714e53fd227abc4749a57de503e35ddf08ea
rafael <eduardo.silva1307@etec.sp.gov.br>
2025-03-21 22:16:09 -0300

    api-naruto

---
+++ constants/Colors.ts
@@ -0,0 +1,26 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintColorLight = '#FFFFFF';
+const tintColorDark = '#FFFFFF';
+
+export const Colors = {
+  light: {
+    text: '#FFFFFF',
+    background: '#FFFFFF',
+    tint: tintColorLight,
+    icon: '#FFFFFF',
+    tabIconDefault: '#FFFFFF',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#FFFFFF',
+    background: '#FFFFFF',
+    tint: tintColorDark,
+    icon: '#FFFFFF',
+    tabIconDefault: '#FFFFFF',
+    tabIconSelected: tintColorDark,
+  },
+};
